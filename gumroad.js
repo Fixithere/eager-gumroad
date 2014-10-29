242b81704061c9bdef470f194be65e8944df1ce4
Nathaniel Blackburn <nblackburn@fastmail.fm>
2014-10-29 23:56:39 +0000

    Initial release

---
+++ gumroad.js
@@ -0,0 +1,13 @@
+window.EagerGumroad = {
+  init: function(options) {
+	(function() {
+		var gumroad   = document.createElement('script');
+		gumroad.type  = 'text/javascript';
+		gumroad.async = options.asynchronous;
+		gumroad.id    = 'gumroad-overlay';
+		gumroad.src = '//gumroad.com/js/gumroad.js';
+		var source = document.getElementsByTagName('script')[0];
+		source.parentNode.insertBefore(gumroad, source);
+	})();
+  }
+};
